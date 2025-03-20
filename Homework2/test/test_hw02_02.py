c342cd4c490fae42292899d42c8bcc019ee29440
Peter Molnar <pmolnar@gsu.edu>
2025-03-20 20:27:27 +0000

    remove submit and test. Using msa8700 package

--- Homework2/test/test_hw02_02.py
+++
@@ -1,7 +0,0 @@
-import os
-import re
-
-def test_hw02_01():
-    assert os.path.isfile('researcher_2.txt'), "Missing output file."
-    lines = open('researcher_2.txt', 'r', encoding='utf-8').readlines()
-    assert len(lines) > 0, "The file is empty."
--- Homework2/test/test_hw02_04.py
+++
@@ -1,7 +0,0 @@
-import os
-import re
-
-def test_hw02_01():
-    assert os.path.isfile('journalist_1.txt'), "Missing output file."
-    lines = open('journalist_1.txt', 'r', encoding='utf-8').readlines()
-    assert len(lines) > 0, "The file is empty."
